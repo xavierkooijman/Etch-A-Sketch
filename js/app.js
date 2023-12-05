532b161b0fc93a8c5e053d0b4222fe580d5510bb
Xavier Kooijman <xavierkooijman@gmail.com>
2023-12-05 21:41:37 +0000

    Fixed function update children in drawing area Range value raised to the power of 2 Adde a function to define the grids columns and rows in relation to the range value Tested with hover in the css to see if the grid was well defined

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 const drawingArea = document.querySelector('#drawingArea')
 const slider = document.querySelector('#slider')
 const sliderText = document.querySelector('#sliderText')
+const colorPicker = document.querySelector('#colorPicker')
 
 slider.addEventListener('input', () => {
   updateSliderText()
+  updateDrawingAreaElements()
   updateDrawingAreaGrid()
 })
 
@@ -12,15 +14,22 @@ function updateSliderText(){
   sliderText.innerText = `${rangeValue} x ${rangeValue}`
 }
 
-function updateDrawingAreaGrid(){
+function updateDrawingAreaElements(){
   drawingArea.replaceChildren()
   let rangeValue = slider.value
-  for(let i=0; i<rangeValue; i++){
+  for(let i=0; i<rangeValue**2; i++){
     const drawingAreaElement = document.createElement('div')
     drawingAreaElement.classList.add('drawingAreaElement')
     drawingArea.appendChild(drawingAreaElement)
   }
 }
 
+function updateDrawingAreaGrid(){
+  let rangeValue = slider.value 
+  drawingArea.style.gridTemplateColumns = "repeat(" + rangeValue + ",1fr)"
+  drawingArea.style.gridTemplateRows = "repeat(" + rangeValue + ",1fr)"
+}
+
 updateSliderText()
+updateDrawingAreaElements()
 updateDrawingAreaGrid()
