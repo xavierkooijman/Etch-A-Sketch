f96d166d7124ded68cb6b3b1c824fe5ff45f3f3a
Xavier Kooijman <xavierkooijman@gmail.com>
2023-12-05 22:49:27 +0000

    Added drawing functionality and change color

--- js/app.js
+++ js/app.js
@@ -3,33 +3,52 @@ const slider = document.querySelector('#slider')
 const sliderText = document.querySelector('#sliderText')
 const colorPicker = document.querySelector('#colorPicker')
 
+let rangeValue = slider.value
+let colorValue = colorPicker.value
+let checkMouseDown = false
+
 slider.addEventListener('input', () => {
-  updateSliderText()
-  updateDrawingAreaElements()
-  updateDrawingAreaGrid()
+  rangeValue = slider.value
+  updateSliderText(rangeValue)
+  updateDrawingAreaElements(rangeValue)
+  updateDrawingAreaGrid(rangeValue)
 })
 
-function updateSliderText(){
-  let rangeValue = slider.value
+colorPicker.addEventListener('input', () => {
+  colorValue = colorPicker.value
+})
+
+document.body.addEventListener('mousedown', () => {
+  checkMouseDown =  true
+})
+
+document.body.addEventListener('mouseup', () => {
+  checkMouseDown =  false
+})
+
+function updateSliderText(rangeValue){
   sliderText.innerText = `${rangeValue} x ${rangeValue}`
 }
 
-function updateDrawingAreaElements(){
+function updateDrawingAreaElements(rangeValue){
   drawingArea.replaceChildren()
-  let rangeValue = slider.value
   for(let i=0; i<rangeValue**2; i++){
     const drawingAreaElement = document.createElement('div')
     drawingAreaElement.classList.add('drawingAreaElement')
+    drawingAreaElement.addEventListener('mouseover', () => {
+      if(checkMouseDown === true){
+        drawingAreaElement.style.backgroundColor = colorValue
+      }
+    })
     drawingArea.appendChild(drawingAreaElement)
   }
 }
 
-function updateDrawingAreaGrid(){
-  let rangeValue = slider.value 
+function updateDrawingAreaGrid(rangeValue){ 
   drawingArea.style.gridTemplateColumns = "repeat(" + rangeValue + ",1fr)"
   drawingArea.style.gridTemplateRows = "repeat(" + rangeValue + ",1fr)"
 }
 
-updateSliderText()
-updateDrawingAreaElements()
-updateDrawingAreaGrid()
+updateSliderText(rangeValue)
+updateDrawingAreaElements(rangeValue)
+updateDrawingAreaGrid(rangeValue)
