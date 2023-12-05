e6cd79c9a33a6185656399519a0631cdbf83d4bb
Xavier Kooijman <xavierkooijman@gmail.com>
2023-12-05 21:03:11 +0000

    function for updating slider text function for updating the drawingArea in relation to the slider value

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-const drawingArea = document.querySelectorAll('#drawingArea')
+const drawingArea = document.querySelector('#drawingArea')
+const slider = document.querySelector('#slider')
+const sliderText = document.querySelector('#sliderText')
+
+slider.addEventListener('input', () => {
+  updateSliderText()
+  updateDrawingAreaGrid()
+})
+
+function updateSliderText(){
+  let rangeValue = slider.value
+  sliderText.innerText = `${rangeValue} x ${rangeValue}`
+}
+
+function updateDrawingAreaGrid(){
+  drawingArea.replaceChildren()
+  let rangeValue = slider.value
+  for(let i=0; i<rangeValue; i++){
+    const drawingAreaElement = document.createElement('div')
+    drawingAreaElement.classList.add('drawingAreaElement')
+    drawingArea.appendChild(drawingAreaElement)
+  }
+}
+
+updateSliderText()
+updateDrawingAreaGrid()
